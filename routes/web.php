ea1ed8f5c3c314ef650eb137b3c10bcd56c03b9d
Unknown <np.carlos@gmail.com>
2018-12-29 23:43:22 -0500

    Remove CodeGenerator Routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get("/generateRelationships", "CodeGenerator@generate");
 
 Route::get('/', function () {
     return view('welcome');
